17dfb89dbcee0a731a49d6432fed1e27cb25b659
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-03-21 12:20:54 +0800

    Add files via upload

---
+++ alien_invasion.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+from settings import Setting
+def run_game():
+	#初始化屏幕并且创建一个屏幕对象
+	pygame.init()    #初始化
+	ai_settings=Setting()
+	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_high))        #设置屏幕大小
+	bg_color=(ai_settings.screen_color)                             #设置背景颜色  
+	pygame.display.set_caption("Super_ship")        #设置屏幕框显示的文字
+	
+	#开始游戏的主循环
+	while True:                                         #每经过一次循环就刷新一下屏幕
+		
+		#监视键盘和鼠标事件
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:              #玩家单击quit，则pygame将侦测到pygame.Quit事件调用sys.exit()来退出
+				sys.exit()
+		#每次循环都会重新绘制屏幕
+		screen.fill(bg_color)
+		#让最近绘制的屏幕可见
+		pygame.display.flip()
+run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Setting():
+	"""将所有关于外星人游戏的设置储存起来"""
+	
+	def __init__(self):                        #构造函数，不需要提供变量，必须包含self
+		#屏幕设置
+		self.screen_width=1200
+		self.screen_high=800
+		self.screen_color=(230,230,230)
