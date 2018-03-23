a79020f2e39b2297ad533b8164ba37b974615f18
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-03-23 19:47:01 +0800

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,16 +1,20 @@
 import pygame
-
+import game_functions as gf
 class Ship():
-	def __init__(self,screen):                                           #ship的构造函数，每次有一个新的对象的时候就会自动在底部中间生成一个小飞船
-		"""初始化飞船，并且设置其初始位置"""
+	def __init__(self,screen):
+		self.move_left=False
+		self.move_right=False
 		self.screen=screen
-		"""加载飞船图像并获取其外接矩形"""
-		self.image=pygame.image.load('images/fly.bmp')
-		self.rect=self.image.get_rect()                                 #加载出来的照片被称为suface，获取suface属性rect
+		self.images=pygame.image.load('images/littlesmall.bmp')
+		self.rect=self.images.get_rect()
 		self.screen_rect=screen.get_rect()
-		"""将每艘飞船放置在屏幕底端"""
+		self.rect.bottom=self.screen_rect.bottom
 		self.rect.centerx=self.screen_rect.centerx
-		self.rect.left=self.screen_rect.left
-	def bliteme(self):
-			"""在指定位置绘制飞船"""
-			self.screen.blit(self.image,self.rect)                 #因为blit函数（在指定位置绘制飞船）与屏幕打印有关所以属于screen
+	def blitme(self):
+		self.screen.blit(self.images,self.rect)
+	def update(self):
+		if self.move_left==True:
+			self.rect.centerx-=1
+		if self.move_right==True:
+			self.rect.centerx+=1
+			
