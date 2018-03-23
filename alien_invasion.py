291ae452706070498632113d830eccb2c49fa5d3
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-03-23 11:01:55 +0800

    a little problem about the picture
    You must use the blitme() after the screen.fill(bg_color),or the bg_color will cover your picture.Interesting!

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Setting
+from ship import Ship
 def run_game():
 	#初始化屏幕并且创建一个屏幕对象
 	pygame.init()    #初始化
@@ -8,7 +9,7 @@ def run_game():
 	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_high))        #设置屏幕大小
 	bg_color=(ai_settings.screen_color)                             #设置背景颜色  
 	pygame.display.set_caption("Super_ship")        #设置屏幕框显示的文字
-	
+	ship=Ship(screen)                               #将上面的screen对象传递给ship() 
 	#开始游戏的主循环
 	while True:                                         #每经过一次循环就刷新一下屏幕
 		
@@ -18,6 +19,7 @@ def run_game():
 				sys.exit()
 		#每次循环都会重新绘制屏幕
 		screen.fill(bg_color)
+		ship.blitme()
 		#让最近绘制的屏幕可见
 		pygame.display.flip()
 run_game()
