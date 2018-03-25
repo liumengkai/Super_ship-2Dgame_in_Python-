77a571c5390e0e1ac4413425c127704d2f1245db
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-03-25 17:01:37 +0800

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,20 +1,25 @@
 import pygame
 import game_functions as gf
+from settings import Setting
 class Ship():
-	def __init__(self,screen):
-		self.move_left=False
-		self.move_right=False
+	def __init__(self,ai_setting,screen):                    #类中的函数必须是要有self的
 		self.screen=screen
-		self.images=pygame.image.load('images/littlesmall.bmp')
+		self.ai_settings=Setting()
+		self.images=pygame.image.load('images/small.bmp')
 		self.rect=self.images.get_rect()
 		self.screen_rect=screen.get_rect()
 		self.rect.bottom=self.screen_rect.bottom
 		self.rect.centerx=self.screen_rect.centerx
+		self.center=float(self.rect.centerx)                 ##因为rect的centerx只能存储整数所以需要在飞船的center属性中存储小数
+		self.move_left=False
+		self.move_right=False
+		#在正确位置画出飞船
 	def blitme(self):
 		self.screen.blit(self.images,self.rect)
+		#更新屏幕
 	def update(self):
-		if self.move_left==True:
-			self.rect.centerx-=1
-		if self.move_right==True:
-			self.rect.centerx+=1
-			
+		if self.move_left and self.rect.left>self.screen_rect.left:
+			self.center-=self.ai_settings.ship_speed_factor
+		if self.move_right and self.rect.right<self.screen_rect.right:
+			self.center+=self.ai_settings.ship_speed_factor
+		self.rect.centerx=self.center
