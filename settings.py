65a635742664a6268d1d7286743473c84895683a
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-03-28 23:17:21 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,11 +4,14 @@ class Setting():
 	def __init__(self):                        #构造函数，不需要提供变量，必须包含self
 		#屏幕设置
 		self.screen_width=1500
-		self.screen_high=835
+		self.screen_height=835
 		self.screen_color=(230,230,230)
 		self.ship_speed_factor=1.5 
 		self.bullet_speed_factor=1.9
 		self.bullet_width=3
 		self.bullet_height=15
 		self.bullet_color=(60,60,60)
-		self.bullet_allowed=5
+		self.bullet_allowed=3
+		self.alien_speed_factor=1
+		self.fleet_direction=1
+		self.fleet_drop_speed= -1
