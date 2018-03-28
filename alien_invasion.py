acf15af6121d6106b3ee0d39c6eb96888ddc7703
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-03-28 23:15:26 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,16 +9,19 @@ def run_game():
 	#初始化屏幕并且创建一个屏幕对象
 	pygame.init()    #初始化
 	ai_settings=Setting()
-	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_high))        #创建了一个屏幕对象screen设置屏幕大小
+	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))        #创建了一个屏幕对象screen设置屏幕大小
 	bg_color=(ai_settings.screen_color)                             #设置背景颜色  
 	pygame.display.set_caption("Super_ship")        #设置屏幕框显示的文字
 	ship = Ship(ai_settings,screen)                              #创建一艘飞船，不可以放在while循环中
+	ship_height=ship.rect.height
 	bullets=Group()
-	alien = Alien(screen)
-	#开始游戏的主循环
+	aliens=Group()
+	gf.creat_fleet(ai_settings,screen,aliens,ship_height)
+		#开始游戏的主循环
 	while True:                                         #每经过一次循环就刷新一下屏幕
 		#监视键盘和鼠标事件
 		gf.check_events(ai_settings,screen,ship,bullets)
 		ship.update()
-		gf.update_screen(ai_settings,screen,ship,bullets,alien)                #每次循环都会重新绘制屏幕 #必须先进行填充背景色#将飞船放置在底端中间#让最近绘制的屏幕可见		                         		
+		gf.update_aliens(ai_settings,aliens,screen)
+		gf.update_screen(ai_settings,screen,ship,bullets,aliens)                #每次循环都会重新绘制屏幕 #必须先进行填充背景色#将飞船放置在底端中间#让最近绘制的屏幕可见		                         		
 run_game()
