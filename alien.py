b627b65bd5cb5034a9b940e9fc67608900c67b9b
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-03-28 23:14:47 +0800

    Update alien.py

--- alien.py
+++ alien.py
@@ -2,13 +2,24 @@ import pygame
 from pygame.sprite import Sprite
 class Alien(Sprite):
 	"""表示单个外星人的类"""
-	def __init__(self,screen):
+	def __init__(self,ai_settings,screen):
 		super().__init__()
 		self.screen=screen
-		self.images=pygame.image.load('images/smallalien.bmp')
-		self.rect=self.images.get_rect()
+		self.ai_settings=ai_settings
+		self.image=pygame.image.load('images/smallalien.bmp')           #在这里传入图形的时候必须叫做image，不可以叫做其他的名字否则不可以用pygame中的draw等自带函数
+		self.rect=self.image.get_rect()                                 #将图像返回成rect()
 		self.rect.x=self.rect.width
 		self.rect.y=self.rect.height
 		self.x=float(self.rect.x)
-	def blitme(self,screen):
-		self.screen.blit(self.images,self.rect);
+	#def blitme(self):
+		#self.screen.blit(self.image,self.rect);
+	def check_edges(self,screen):
+		screen_rect=self.screen.get_rect()
+		if(self.rect.right>=screen_rect.right):
+			return True
+		elif self.rect.left<=0:
+			 return True
+	def update(self,ai_settings):
+		self.x+=(ai_settings.alien_speed_factor*self.ai_settings.fleet_direction)                   #self.rect.x是只能存储实数的变量
+		self.rect.x=self.x
+		
