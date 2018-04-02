bb8f9955a67af04149485cbaaaa7561d66954352
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-04-02 22:19:00 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 class Setting():
 	"""将所有关于外星人游戏的设置储存起来"""
 	
@@ -7,11 +8,11 @@ class Setting():
 		self.screen_height=835
 		self.screen_color=(230,230,230)
 		self.ship_speed_factor=1.5 
-		self.bullet_speed_factor=1.9
+		self.bullet_speed_factor=3
 		self.bullet_width=3
 		self.bullet_height=15
 		self.bullet_color=(60,60,60)
 		self.bullet_allowed=3
 		self.alien_speed_factor=1
-		self.fleet_direction=1
-		self.fleet_drop_speed= -1
+		self.fleet_direction=1                   #方向表示
+		self.fleet_drop_speed=10         
